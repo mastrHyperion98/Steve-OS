4847b01784cee8e04ede57842a39bd97b6c7bcae
Steven Smith <s_smith1998@outlook.com>
2020-08-21 23:38:01 -0400

    bootable sector.

--- boot/bootsector.asm
+++ boot/bootsector.asm
@@ -3,3 +3,10 @@
 ; Steve OS Boot Sector
 ; Code and Project under the GPL3 licence
 
+USE16;denotes to nasm that the code is in 16 bit. 
+
+jmp $
+; padd the label to the end of the bootsector
+times 510-($-$$) db 0 
+dw 0xaa55   ; write bootable label in the sector. 
+
